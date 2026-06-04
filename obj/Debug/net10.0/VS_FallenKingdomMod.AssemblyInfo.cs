d89283395937413e9745cd6889cb075d51b26304
Hugol4ka <hugolaca12300@gmail.com>
2026-06-04 15:10:30 +0200

    Fix errors no args + fix message"Objectif"

--- obj/Debug/net10.0/VS_FallenKingdomMod.AssemblyInfo.cs
+++ obj/Debug/net10.0/VS_FallenKingdomMod.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VS_FallenKingdomMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f3426e7a37e56d8514892f2b3edfa7ac850b83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2f7eaf2044e23cd3eb9196686efae915d9e1356")]
 [assembly: System.Reflection.AssemblyProductAttribute("VS_FallenKingdomMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VS_FallenKingdomMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
